099c24860e62baa8e24e11b88c2227ebdebfcd54
brian-tura <b.tura2002@gmail.com>
2025-04-07 15:00:33 +0200

    [FEAT] Finished overlay feature

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
         console.log(photos);
         for (let i = 0; i < photos.length; i++) {
             let singlePhoto = `<div class="bg-white col-8 p-3 col-md-5 col-lg-3 my-3 mx-2 photo-holder d-flex flex-column align-items-center">
-                                    <img class="img-fluid mb-2" src="${photos[i].url}" alt="">
+                                    <img class="img-fluid mb-2" onClick="display()" src="${photos[i].url}" alt="">
                                     <img class="pin" src="./img/pin.svg" alt="">
                                     <p class="align-self-start date">${photos[i].date}</p>
                                     <h5 class="align-self-start"> ${photos[i].title}</h5>    
@@ -14,3 +14,21 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
             photoContainer.innerHTML += singlePhoto;
         }
     })
+
+
+let overlay = document.getElementById('overlay');
+let overlayHolder = document.getElementById('overlay-holder');
+
+function display(){
+    overlay.style.display = "flex";
+    let placeHolder = overlayHolder.innerHTML
+    overlayHolder.innerHTML = `<img class="img-fluid" src="${event.target.src}" alt="">` + overlayHolder.innerHTML;
+    
+    let button = document.getElementById('closer');
+    button.addEventListener("click", () => {
+        overlay.style.display = "none";
+        overlayHolder.innerHTML = placeHolder;
+    })
+}
+
+
